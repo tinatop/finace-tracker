2e0c5d35a97b51f70916db5a87ee6b2fa480c74e
mtopalovic <martina.topalovic@a1.hr>
2022-12-16 14:54:58 +0100

    added chart

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,38 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditig] = useState(false);
+
   const saveExpenseDataHandler = (enterdExpenseData) => {
     const expenseData = {
       ...enterdExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditig(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditig(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditig(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
