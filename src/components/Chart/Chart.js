ff55b640bc63f07ec74ab3ee50ba4f234b911aff
mtopalovic <martina.topalovic@a1.hr>
2022-12-16 14:53:49 +0100

    added chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar value={dataPoint.value} maxValue={null}  />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
